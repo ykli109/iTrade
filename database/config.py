1c6d483b72cac103a5702f01265c3684b11f48d7
Alan.Li <ykli109@outlook.com>
2024-11-09 00:24:09 +0800

    fix: .env文件缓存问题

--- database/config.py
+++ database/config.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 # 加载 .env 文件中的环境变量
-load_dotenv()
+load_dotenv(override=True)
 
 DB_CONFIGS = {
     'default': {
@@ -15,12 +15,4 @@ DB_CONFIGS = {
         "port": int(os.getenv("DB_PORT", 3306)),
         "charset": "utf8mb4"
     },
-    # 'analytics': {
-    #     "host": os.getenv("DB_ANALYTICS_HOST", "localhost"),
-    #     "user": os.getenv("DB_ANALYTICS_USER", "root"),
-    #     "password": os.getenv("DB_ANALYTICS_PASSWORD", "password"),
-    #     "database": os.getenv("DB_ANALYTICS_DATABASE", "itrade_analytics"),
-    #     "port": int(os.getenv("DB_ANALYTICS_PORT", 3306)),
-    #     "charset": "utf8mb4"
-    # }
 }
